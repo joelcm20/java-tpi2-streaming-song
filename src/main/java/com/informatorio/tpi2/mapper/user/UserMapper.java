afe199a6c39bdb5251d8c84cceee02e335eaa932
joelcm20 <cjoel1977@gmail.com>
2023-12-17 12:30:48 -0300

    user mapper

---
+++ src/main/java/com/informatorio/tpi2/mapper/user/UserMapper.java
@@ -0,0 +1,41 @@
+package com.informatorio.tpi2.mapper.user;
+
+import com.informatorio.tpi2.domain.PlayList;
+import com.informatorio.tpi2.domain.User;
+import com.informatorio.tpi2.dto.user.UserDto;
+import com.informatorio.tpi2.service.playList.PlayListService;
+import lombok.AllArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+public class UserMapper {
+    private PlayListService playListService;
+
+    public static UserDto mapToUserDto(User user) {
+        UserDto userDto = new UserDto();
+        userDto.setName(user.getName());
+        userDto.setUsername(user.getUsername());
+        for (PlayList pl : user.getPlayLists()) {
+            userDto.getPlayLists().add(pl.getId());
+        }
+        return userDto;
+    }
+
+    public static List<UserDto> mapToUserDto(List<User> users) {
+        List<UserDto> userDtos = new ArrayList<>();
+        for (User user : users) {
+            userDtos.add(mapToUserDto(user));
+        }
+        return userDtos;
+    }
+
+    public User mapToUser(UserDto userDto) {
+        User user = new User();
+        user.setName(userDto.getName());
+        user.setUsername(userDto.getUsername());
+        user.setPlayLists(playListService.findByIds(userDto.getPlayLists()));
+        return user;
+    }
+}
