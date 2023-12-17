f1a7314f910e9f97c5e13fbbdf28fb4d7a2a036e
joelcm20 <cjoel1977@gmail.com>
2023-12-17 12:29:13 -0300

    some fixes on bootstrap data

--- src/main/java/com/informatorio/tpi2/bootstrap/BootstrapData.java
+++ src/main/java/com/informatorio/tpi2/bootstrap/BootstrapData.java
@@ -14,7 +14,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Component
