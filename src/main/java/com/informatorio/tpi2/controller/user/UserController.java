2cecd58deb89d48de02b1768756b849180dc6132
joelcm20 <cjoel1977@gmail.com>
2023-12-17 13:34:00 -0300

    user controller: create user

--- src/main/java/com/informatorio/tpi2/controller/user/UserController.java
+++ src/main/java/com/informatorio/tpi2/controller/user/UserController.java
@@ -3,21 +3,19 @@ package com.informatorio.tpi2.controller.user;
 import com.informatorio.tpi2.domain.User;
 import com.informatorio.tpi2.dto.user.UserDto;
 import com.informatorio.tpi2.mapper.user.UserMapper;
+import com.informatorio.tpi2.repository.user.UserRepository;
 import com.informatorio.tpi2.service.user.UserService;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/user")
 public class UserController {
     private UserService userService;
+    private UserRepository userRepository;
 
     @GetMapping
     public List<UserDto> getUsers() {
@@ -27,5 +25,12 @@ public class UserController {
     @GetMapping("{username}")
     public UserDto getUsername(@PathVariable(name = "username") String username) {
         return UserMapper.mapToUserDto(userService.findByUsername(username));
+    }
+
+    @PostMapping
+    public String createUser(@RequestBody UserDto userBody) {
+        User newUser = UserMapper.mapToUser(userBody);
+        userRepository.save(newUser);
+        return "user created";
     }
 }
