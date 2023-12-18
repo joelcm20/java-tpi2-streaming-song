7b8fbe4f3d12b315f49ab8bca7d9796913e419dd
joelcm20 <cjoel1977@gmail.com>
2023-12-18 13:39:21 -0300

    some fixes on user controller

--- src/main/java/com/informatorio/tpi2/controller/user/UserController.java
+++ src/main/java/com/informatorio/tpi2/controller/user/UserController.java
@@ -1,11 +1,17 @@
 package com.informatorio.tpi2.controller.user;
 
+import com.informatorio.tpi2.constants.ConstantUtils;
 import com.informatorio.tpi2.domain.User;
+import com.informatorio.tpi2.dto.responses.get.GetUserResponseDto;
+import com.informatorio.tpi2.dto.responses.get.GetUsersResponseDto;
+import com.informatorio.tpi2.dto.responses.post.CreateUserResponseDto;
 import com.informatorio.tpi2.dto.user.UserDto;
 import com.informatorio.tpi2.mapper.user.UserMapper;
 import com.informatorio.tpi2.repository.user.UserRepository;
 import com.informatorio.tpi2.service.user.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,19 +24,21 @@ public class UserController {
     private UserRepository userRepository;
 
     @GetMapping
-    public List<UserDto> getUsers() {
-        return UserMapper.mapToUserDto(userService.findAll());
+    public ResponseEntity<GetUsersResponseDto> getUsers() {
+        List<UserDto> users = UserMapper.mapToUserDto(userService.findAll());
+        return ResponseEntity.status(HttpStatus.OK).body(new GetUsersResponseDto(ConstantUtils.STATUS_200, users));
     }
 
     @GetMapping("{username}")
-    public UserDto getUsername(@PathVariable(name = "username") String username) {
-        return UserMapper.mapToUserDto(userService.findByUsername(username));
+    public ResponseEntity<GetUserResponseDto> getUserByUsername(@PathVariable(name = "username") String username) {
+        UserDto user = UserMapper.mapToUserDto(userService.findByUsername(username));
+        return ResponseEntity.status(HttpStatus.OK).body(new GetUserResponseDto(ConstantUtils.STATUS_200, user));
     }
 
     @PostMapping
-    public String createUser(@RequestBody UserDto userBody) {
-        User newUser = UserMapper.mapToUser(userBody);
-        userRepository.save(newUser);
-        return "user created";
+    public ResponseEntity<CreateUserResponseDto> createUser(@RequestBody UserDto userBody) {
+        User newUser = userRepository.save(UserMapper.mapToUser(userBody));
+        UserDto userDto = UserMapper.mapToUserDto(newUser);
+        return ResponseEntity.status(HttpStatus.CREATED).body(new CreateUserResponseDto(ConstantUtils.STATUS_201, userDto));
     }
 }
