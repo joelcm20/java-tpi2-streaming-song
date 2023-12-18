396727b612ee4ecd8fc53e722483a4868cbb618b
joelcm20 <cjoel1977@gmail.com>
2023-12-18 13:37:23 -0300

    some fixes on user services

--- src/main/java/com/informatorio/tpi2/service/user/IUserService.java
+++ src/main/java/com/informatorio/tpi2/service/user/IUserService.java
@@ -1,6 +1,8 @@
 package com.informatorio.tpi2.service.user;
 
 import com.informatorio.tpi2.domain.User;
+import com.informatorio.tpi2.dto.user.UserDto;
+import com.informatorio.tpi2.exception.NotFoundException;
 
 import java.util.List;
 import java.util.UUID;
@@ -8,7 +10,7 @@ import java.util.UUID;
 public interface IUserService {
     User findById(UUID id);
 
-    User findByUsername(String username);
+    User findByUsername(String username) throws NotFoundException;
 
     List<User> findAll();
 }
--- src/main/java/com/informatorio/tpi2/service/user/UserService.java
+++ src/main/java/com/informatorio/tpi2/service/user/UserService.java
@@ -1,11 +1,13 @@
 package com.informatorio.tpi2.service.user;
 
 import com.informatorio.tpi2.domain.User;
+import com.informatorio.tpi2.exception.NotFoundException;
 import com.informatorio.tpi2.repository.user.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.UUID;
 
 @Service
@@ -15,12 +17,16 @@ public class UserService implements IUserService {
 
     @Override
     public User findById(UUID id) {
-        return userRepository.findById(id).orElseThrow(() -> new RuntimeException("user not found"));
+        return userRepository.findById(id).orElseThrow(() -> new NotFoundException("Usuario", "id", id.toString()));
     }
 
     @Override
-    public User findByUsername(String username) {
-        return userRepository.findByUsername(username);
+    public User findByUsername(String username) throws NotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (Objects.isNull(user)) {
+            throw new NotFoundException("Usuario", "username", username);
+        }
+        return user;
     }
 
     @Override
