2424cad4a31db5e7eb710363fe01850b70102a45
joelcm20 <cjoel1977@gmail.com>
2023-12-18 13:40:08 -0300

    create constats utils

---
+++ src/main/java/com/informatorio/tpi2/constants/ConstantUtils.java
@@ -0,0 +1,14 @@
+package com.informatorio.tpi2.constants;
+
+import java.security.PublicKey;
+
+public class ConstantUtils {
+    public static final String STATUS_201 = "201";
+    public static final String MESSAGE_201 = "Recurso creado satisfactoriamente";
+    public static final String STATUS_200 = "200";
+    public static final String MESSAGE_200 = "Request procesada satisfactoriamente";
+    public static final String STATUS_404 = "404";
+    public static final String MESSAGE_404 = "Recurso no encontrado";
+    public static final String STATUS_500 = "500";
+    public static final String MESSAGE_500 = "Un error a ocurrido. Prueba de nuevo o contacta al equipo de desarrollo.";
+}
