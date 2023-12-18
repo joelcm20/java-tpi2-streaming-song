1f40332600a4150a204765718a1c0ccb0d3d3c1d
joelcm20 <cjoel1977@gmail.com>
2023-12-18 13:34:57 -0300

    create exception handler

---
+++ src/main/java/com/informatorio/tpi2/exception/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.informatorio.tpi2.exception;
+
+import com.informatorio.tpi2.constants.ConstantUtils;
+import com.informatorio.tpi2.dto.error.ErrorResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<ErrorResponseDto> handleNotFoundException(NotFoundException exception, WebRequest webRequest) {
+        ErrorResponseDto errorResponse = new ErrorResponseDto();
+        errorResponse.setApiPath(webRequest.getDescription(false));
+        errorResponse.setErrorMessage(exception.getMessage());
+        errorResponse.setHttpStatus(HttpStatus.NOT_FOUND);
+        errorResponse.setErrorTime(LocalDateTime.now());
+        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+    }
+}
---
+++ src/main/java/com/informatorio/tpi2/exception/NotFoundException.java
@@ -0,0 +1,27 @@
+package com.informatorio.tpi2.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException {
+    public NotFoundException(String nombreDeRecurso, String nombreDeAtributo, String valorDeAtributo) {
+        super(String.format("%s: No se encuentra el recurso con el dato de entrada '%s': %s", nombreDeRecurso, nombreDeAtributo, valorDeAtributo));
+    }
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public NotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public NotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
